2416d6c65440f4af60965dcb5e484119d683da2f
Yan <yan_salim@hotmail.com>
2020-11-13 12:43:16 -0300

    redes sociais

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,9 @@
 function Home () {
     <div>Me siga nas minhas redes sociais</div>
     return <a href="facebook.com/yansalim23">Facebook</a>
+    <a target="_blank">Link<a/>
+
+
                 
 }
 export default Home
