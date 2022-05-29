3477e50aa3be731b4f9d3378e314a9196eeea221
yansalim <yan_salim@hotmail.com>
2022-05-29 20:19:27 -0300

    att console

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,13 @@
   
 function Home () {
 
-    return <div>Projeto dev em andamento. Acompanhe tudo em 
-        <a href="twitch.tv/yansalim23" >twitch.tv/yansalim23</a> </div>
+
+    var hotText = TWITCH;
+    var URL = 'twitch.tv/yansalim';
+
+    console.log ('Projeto em andamento. Acompanhe tudo no meu canal na' + hotText.link(URL) );
+    return //<div>Projeto dev em andamento. Acompanhe tudo em 
+       // <a href="twitch.tv/yansalim23" >twitch.tv/yansalim23</a> </div>
 
 
                 
