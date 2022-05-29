7b712145b1f30d23e6e3df7e3f4f1cf4a33e8890
yansalim <yan_salim@hotmail.com>
2022-05-29 20:53:44 -0300

    att link div

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ function Home () {
 
 
     
-    return <div>Projeto dev em andamento. Acompanhe tudo em 
+    return <div>Projeto dev em andamento. Acompanhe tudo em <link> twitch.tv/yansalim23</link>
         <a href="twitch.tv/yansalim23" > twitch.tv/yansalim23</a> </div>
 
 
