77fa45dc77442fe953aca205f95a8380797caa44
yansalim <yan_salim@hotmail.com>
2022-05-29 20:11:42 -0300

    att hlink

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@
 function Home () {
 
     return <div>Projeto dev em andamento. Acompanhe tudo em 
-        <a href="twitch.tv/yansalim23" ></a> </div>
+        <a href="twitch.tv/yansalim23" ></a> twitch.tv/yansalim23 </div>
 
 
                 
