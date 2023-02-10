fcb0339ab896b05dec2892710ccf27f98938c174
Yan Salim <yan_salim@hotmail.com>
2023-02-10 16:11:16 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ function Home () {
 
 
     
-    return <div>Projeto dev em andamento. Acompanhe tudo em twitch.tv/yansalim23</div>
+    return <div>Projeto dev em andamento. Acompanhe tudo em twitch.tv/yansalimdev</div>
        // <a href="twitch.tv/yansalim23" > twitch.tv/yansalim23</a>
 
 
